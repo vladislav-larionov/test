9a83ff245361d31caba79cc1c1b2a6434b8e82ed
Ларионов Владислав Дмитриевич <Larionov@WUD.lan>
2019-10-03 13:51:48 +0300

    Add the 2 digit to hello world output

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("A");
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world!2");
 		System.out.println("B");
 	}
 }
